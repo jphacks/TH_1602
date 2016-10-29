c3b13e812e4b666665da31413cce92a7b9c9e51f
Mizuki Sango <msgeha@gmail.com>
2016-10-29 22:18:47 +0900

    regisration post

--- client/src/pages/object/registration/object-registration.ts
+++ client/src/pages/object/registration/object-registration.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
 import { ObjectTagApi } from '../../../api/api/ObjectTagApi';
 import { MyApp } from '../../../app/app.component';
@@ -11,10 +11,11 @@ import { MyApp } from '../../../app/app.component';
 
 export class ObjectRegistrationPage {
 
-  register = {}
+  register = {item: ''}
   error = false
-  constructor(public navCtrl: NavController, public params: NavParams) {
-    console.log('ok')
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public params: NavParams) {
+    console.log(this.register.item.length)
   }
 
   private get api(): ObjectTagApi {
@@ -22,14 +23,31 @@ export class ObjectRegistrationPage {
   }
 
   public post () {
-    console.log('post')
-    console.log(this.register)
-    var log = this.api.objectTagsPost(this.register).toPromise().then((response) => {
+    if (this.register.item) {
+      console.log('post')
+      var response = this.api.objectTagsPost(this.register).toPromise().then((response) => {
+        this.navCtrl.pop //うまく言ったときの処理
+      }).catch(reason => { //エラー吐いたときの処理
+      if (reason.status !== 0) {
+        this.showAlert('サーバーエラー', 'サーバーの管理者に問い合わせてください');
+      }else {
+        this.showAlert('ネットワークエラー', 'インターネットに接続されているか，確認してください');
+      }
+      })
+    }
+    else {
+      console.log('not post')
+      return undefined;
+    }
+  }
 
-    }).catch(() => {
-      this.error = true;
-    })
-    console.log(log)
+  showAlert(title: string, subTitle: string) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subTitle,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
