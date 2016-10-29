ca1ead6030c1b76a625d045b856da99d80da39ac
Jumpei Kaizu <jumpei@MacBook-Air.local>
2016-10-30 00:41:41 +0900

    叩き台

--- client/src/pages/user/details/user-details.ts
+++ client/src/pages/user/details/user-details.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { UserInfoResponse, UserInfoApi, ObjectTagResponse, ObjectTagApi, ReservationApi } from '../../../api/';
+import { MyApp } from '../../../app/app.component';
 
 @Component({
   selector: 'page-user-details',
@@ -11,5 +13,15 @@ export class UserDetailsPage {
   constructor(public navCtrl: NavController) {
 
   }
+  private get userApi(): UserInfoApi {
+    return MyApp.injector.get(UserInfoApi);
+  }
 
+  private get objectApi(): ObjectTagApi {
+    return MyApp.injector.get(ObjectTagApi);
+  }
+
+  private get reservationApi(): ReservationApi {
+    return MyApp.injector.get(ReservationApi);
+  }
 }
