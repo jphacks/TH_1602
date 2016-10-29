1cd3e916791676478563a8f9507a6d31b36d22fb
Mizuki Sango <msgeha@gmail.com>
2016-10-29 18:34:13 +0900

    make registration post

--- client/src/pages/object/registration/object-registration.ts
+++ client/src/pages/object/registration/object-registration.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
+import { ObjectTagApi } from '../../../api/api/ObjectTagApi';
+import { MyApp } from '../../../app/app.component';
 
 @Component({
   selector: 'page-object-registration',
   templateUrl: 'object-registration.html'
 })
+
 export class ObjectRegistrationPage {
 
-  constructor(public navCtrl: NavController) {
+  register = {}
+  error = false
+  constructor(public navCtrl: NavController, public params: NavParams) {
+    console.log('ok')
+  }
 
+  private get api(): ObjectTagApi {
+    return MyApp.injector.get(ObjectTagApi)
+  }
+
+  public post () {
+    console.log('post')
+    console.log(this.register)
+    var log = this.api.objectTagsPost(this.register).toPromise().then((response) => {
+
+    }).catch(() => {
+      this.error = true;
+    })
+    console.log(log)
   }
 
 }
