444115c17fcc8f156ef38701f10eaf28acaea6e4
oboenikui <oboenikuitwitter@gmail.com>
2016-10-29 19:22:47 +0900

    オブジェクト名を表示するようにした

--- client/src/pages/object/details/object-details.ts
+++ client/src/pages/object/details/object-details.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import { CategoryResponse, CategoryApi, ObjectTagResponse, ObjectTagApi, ReservationApi } from '../../../api/';
+import { MyApp } from '../../../app/app.component';
 
 @Component({
   selector: 'page-object-details',
@@ -9,9 +11,32 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ObjectDetailsPage {
   catId: number;
   objId: string;
+  objTag: ObjectTagResponse;
+  category: CategoryResponse;
   constructor(public navCtrl: NavController, private navParams: NavParams) {
     this.catId = navParams.get("catid");
     this.objId = navParams.get("objid");
+    this.objTag = navParams.get("object_tag");
+    this.category = navParams.get("category");
+    if(!this.objTag) {
+      this.objectApi.objectTagsGet(this.objId).toPromise().then(data => {
+        this.objTag = data;
+      }).catch(reason => {
+        
+      })
+    }
+  }
+
+  private get categoryApi(): CategoryApi {
+    return MyApp.injector.get(CategoryApi);
+  }
+
+  private get objectApi(): ObjectTagApi {
+    return MyApp.injector.get(ObjectTagApi);
+  }
+
+  private get reservationApi(): ReservationApi {
+    return MyApp.injector.get(ReservationApi);
   }
 
 }
