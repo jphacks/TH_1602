a1773a908cf62ef16c12da7260dd002d4234fb90
Jumpei Kaizu <jumpei@MacBook-Air.local>
2016-10-30 00:02:50 +0900

    ユーザーリストの作成

--- client/src/pages/user/list/user-list.ts
+++ client/src/pages/user/list/user-list.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Refresher } from 'ionic-angular';
+import { UserInfoResponse, UserInfoApi } from '../../../api/';
+import { MyApp } from '../../../app/app.component';
+import { UserDetailsPage } from '../details/user-details';
 
 @Component({
   selector: 'page-user-list',
   templateUrl: 'user-list.html'
 })
 export class UserListPage {
+  error: boolean = false;
+  users: Array<UserInfoResponse> = null;
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    let loader = this.loadingCtrl.create({
+      content: "読み込み中..."
+    });
+    loader.present();
+    this.userApi.listUsersGet().toPromise()
+      .then(data => {
+        this.users = data.items;
+        this.error = false;
+        loader.dismiss();
+      }).catch(reason => {
+        this.error = true;
+        loader.dismiss();
+      })
+  }
 
-  constructor(public navCtrl: NavController) {
+  private get userApi(): UserInfoApi {
+    return MyApp.injector.get(UserInfoApi);
+  }
 
+  doRefresh(refresher: Refresher) {
+    this.userApi.listUsersGet().toPromise().then(data => {
+      this.users = data.items;
+      this.error = false;
+      refresher.complete();
+    }).catch(reason => {
+      this.error = true;
+      refresher.complete();
+    });
+  }
+
+  push(usr: UserInfoResponse) {
+    this.navCtrl.push(UserDetailsPage, {
+      usrname: usr.userName,
+    });
   }
 
 }
+
