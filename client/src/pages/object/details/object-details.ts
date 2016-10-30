c82459ef6028dea22ac7463a9db6fabe6cd64d27
oboenikui <oboenikuitwitter@gmail.com>
2016-10-30 14:04:53 +0900

    fix Android compile error

--- client/src/pages/object/details/object-details.ts
+++ client/src/pages/object/details/object-details.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-import { NavController, NavParams, LoadingController, Refresher } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Refresher, AlertController } from 'ionic-angular';
 import { CategoryResponse, CategoryApi, ObjectTagResponse, ObjectTagApi, ReservationApi, ReservationResponse } from '../../../api/';
 import { MyApp } from '../../../app/app.component';
 import { MyErrorCard } from '../../../components/error-card';
@@ -19,7 +19,7 @@ export class ObjectDetailsPage {
   serverError = false;
   networkError = false;
   imgError = false;
-  constructor(public navCtrl: NavController, private navParams: NavParams, private loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
     this.catId = navParams.get("catid");
     this.objId = navParams.get("objid");
     this.objTag = navParams.get("object_tag");
@@ -29,6 +29,7 @@ export class ObjectDetailsPage {
     });
     loader.present();
     this.load(() => loader.dismiss());
+    this.checkInTime();
   }
 
   load(finish?: () => any) {
@@ -51,13 +52,13 @@ export class ObjectDetailsPage {
     this.reservationApi.searchReservationsGet(this.objTag.id).toPromise()
       .then(data => {
         this.reservations = data.items.sort((a, b) => {
-          if(this.objTag.bookingEnabled) {
+          if (this.objTag.bookingEnabled) {
             return a.startAt.valueOf() - b.startAt.valueOf()
           } else {
             return a.createdAt.valueOf() - b.createdAt.valueOf()
           }
         });
-        if(this.reservations[0].isActive) {
+        if (this.reservations[0].isActive) {
           this.currentReservation = this.reservations.shift();
         }
         finish && finish();
@@ -87,13 +88,53 @@ export class ObjectDetailsPage {
   onImgError(event: UIEvent) {
     this.imgError = true;
   }
+
+  checkIn() {
+
+  }
+
+  checkInTime() {
+    let alert = this.alertCtrl.create({
+      title: '今すぐ使用',
+      message: 'いつまで使用するかを入力してください．',
+      inputs: [
+        {
+          name: 'time',
+          placeholder: '時間',
+          type: 'time'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            data["time"];
+          }
+        }
+      ]
+    });
+    alert.present();
+
+  }
+
+  reservate() {
+
+  }
+
+
 }
 
 @Component({
   selector: 'my-user-item',
   template:
   `<ion-item>
-    <div class="users">{{_reservationusers[0].name}}</div>
+    <div class="users">{{_reservation.users[0].name}}</div>
     <div class="time" *ngIf="_reservation.startAt"><span> {{_reservation.startAt | date: 'yyyy/MM/dd hh:mm'}} </span>&nbsp;~&nbsp;<span *ngIf="_reservation.endAt"> {{_reservation.endAt | date: 'yyyy/MM/dd hh:mm'}} </span></div>
   </ion-item>`
 })
