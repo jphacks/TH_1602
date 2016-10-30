7e45ebfcfca963b2fe56df4df0f8e8f378998341
sukerutulo <sukerutulo+github@gmail.com>
2016-10-30 14:01:45 +0900

    自分の予約している物品の詳細ページへの導線を追加

--- client/src/pages/user/details/user-details.ts
+++ client/src/pages/user/details/user-details.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UserInfoResponse, UserInfoApi, ObjectTagResponse, ObjectTagApi, ReservationResponse, ReservationApi } from '../../../api/';
 import { MyApp } from '../../../app/app.component';
+import { ObjectDetailsPage } from '../../details/object-details';
 
 @Component({
   selector: 'page-user-details',
@@ -19,20 +20,8 @@ export class UserDetailsPage {
   constructor(public navCtrl: NavController, private navParams: NavParams) {
     this.userName = navParams.get("username");
     this.userInfo = navParams.get("userinfo");
-    
-    /* userInfoGetは存在しない
-    if(!this.userInfo) {
-      this.userApi.userInfoGet(this.userName).toPromise().then(data => {
-        this.userInfo = data;
-      }).catch(reason => {
-        this.error = true;
-      })
-    }
-    */
 
     this.reservationApi.searchReservationsGet(null, this.userName).toPromise().then((response) => {
-      // 今使っているかどうかを判定
-      // 使ってるときはusings, まだ使ってないときはreservations
       let now = new Date();
       for(let r of response.items){
         let resStart = new Date(r.startAt); 
@@ -42,13 +31,24 @@ export class UserDetailsPage {
         else{
           this.reservations.push(r);
         }
-      }
-      
+      }      
     }).catch(() => {
       this.error = true;
     })
   }
-  
+
+  push(res: ReservationResponse) {
+    let objid = res.objectTag.id; 
+    this.objectApi.objectTagsIdGet(objid).toPromise().then((obj) => {
+      this.navCtrl.push(ObjectDetailsPage, {
+        catid: obj.category.id,
+        objid: obj.id,
+        object_tag: res.objectTag,
+        category: obj.category
+      });
+    })
+  }
+
   private get userApi(): UserInfoApi {
     return MyApp.injector.get(UserInfoApi);
   }
